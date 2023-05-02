03ef40632e12ad50531bea4acb60d54ea123d0a8
Boody <bodebi639@gmail.com>
2023-05-02 18:56:28 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { Octokit } from "octokit";
+import dotenv from 'dotenv';
+import prompt from 'prompt-sync';
+dotenv.config({
+  path: '../config/.env'
+});
+
+
+
+async function initialize(name, desc, bool) {
+  const input = prompt();
+
+  const octokit = new Octokit({
+    auth: process.env.TOKEN  
+  });
+
+  name = input('Name of Repo: ');
+  desc = input('Description of Repo: ');
+  bool = input('Private? (true/false): ');
+
+  bool.toLowerCase === 'true' ? bool = true : bool = false;
+
+  await octokit.request('POST /user/repos', {
+    name: name,
+    description: desc,
+    homepage: 'https://github.com',
+    'private': bool,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  });
+}
+
+initialize();
